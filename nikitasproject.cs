86106f53b9dcca935e6ca16a8a3d6d7e78e22bbe
nikita12032011 <152791531+nikita12032011@users.noreply.github.com>
2023-12-03 16:56:00 +0300

    Add files via upload

---
+++ nikitasproject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nikitasproject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //€ ввел принт и закрыл скобками
+        print("hello,world");
+        print("hello,world");      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
